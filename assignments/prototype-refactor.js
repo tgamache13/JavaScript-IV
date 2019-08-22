d3722cc7de3a2545a2b13bee5329964a1321fdad
Tara Gamache <tgamache.13@gmail.com>
2019-08-22 11:37:30 -0700

    mvp for prototype refactor

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,105 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class GameObject{
+    constructor(attrs){
+        this.createdAt = attrs.createdAt
+        this.name = attrs.name
+        this.dimensions = attrs.dimensions
+    }
+
+    destroy(){
+        return `${this.name} was removed from the game.`;
+    }
+  }
+
+class CharacterStats extends GameObject{
+    constructor(characterStatsAttrs){
+        super(characterStatsAttrs);
+        this.healthPoints = characterStatsAttrs.healthPoints
+    }
+    takeDamage(){
+        return `${this.name} took damage.`;
+    }
+  }
+  
+  
+ 
+
+class Humanoid extends CharacterStats{
+    constructor(HumanoidAttrs){
+        super(HumanoidAttrs);
+        this.team = HumanoidAttrs.team
+        this.weapons = HumanoidAttrs.weapons
+        this.language = HumanoidAttrs.language   
+    }
+    greet(){
+        return `${this.name} offers a greeting in ${this.language}.`;
+    }
+  }
+
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
+ 
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+  
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+ 
+  
+  
+  
